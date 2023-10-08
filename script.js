37ba4df02b3f9a1b60cc3aa5432448010e720e80
MOHAMED ABDALLA YAHYA <133869739+MoHaMed-Abdlla@users.noreply.github.com>
2023-10-08 22:01:20 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ $(document).ready(function() {
         if ($(this).scrollTop() > 600) {
             // Add the scrolled class to make the navbar fixed
             $('.navbar-brand img').width(125);
+            $(".navbar").removeClass("position-absolute w-100 bg-white");
             $(".navbar").addClass("fixed-top shadow bg-white");
         } else {
             // Remove the scrolled class to make the navbar unfixed
+            $(".navbar").addClass("fixed-top shadow bg-white position-absolute w-100");
             $(".navbar").removeClass("fixed-top shadow bg-white");
             $('.navbar-brand img').width(200)
 
@@ -19,9 +21,11 @@ const fadein = document.querySelectorAll(".fade-in");
 const slidediv = document.querySelectorAll('.slide-div')
 
 const fade = new IntersectionObserver((entries, observer) => {
-    entries.forEach((entry) => {
+    entries.forEach((entry, index) => {
         if (entry.isIntersecting) {
-            entry.target.classList.add('fade-animate');
+            setTimeout(() => {
+                entry.target.classList.add('fade-animate');
+              }, index * 200);
             observer.unobserve(entry.target);
         }
     });
@@ -41,3 +45,58 @@ fadein.forEach((element) => {
 slidediv.forEach((element) => {
     slide.observe(element)
 });
+
+$('.logo-container').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    rtl: true,
+    arrows: false,
+    responsive: [
+        {
+          breakpoint: 600, 
+          settings: {
+            slidesToShow: 2, 
+          }
+        }
+      ]
+  });
+$('#ratings').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    rtl: true,
+    arrows: false,
+    responsive: [
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1 
+          }
+        }
+      ]
+  });
+
+  $('.ratignsleft').click(function(){
+    $('#ratings').slick('slickPrev');
+  });
+
+  $('.ratingsright').click(function(){
+    $('#ratings').slick('slickNext');
+  });
+  $('.logoleft').click(function(){
+    $('.logo-container').slick('slickPrev');
+  });
+
+  $('.logoright').click(function(){
+    $('.logo-container').slick('slickNext');
+  });
+
+  
+  document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll('.slick-dots li button:not([style*="none"])')
+      .forEach(dotButton => dotButton.innerHTML = '');
+  });
+  
